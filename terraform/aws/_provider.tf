41203f99a84ab006aa6d86a5b8fc28eb88f7a979
Arif Akbar <arifakbarkv1@gmail.com>
2026-02-22 20:26:51 +0530

    Segerated pipelines

--- terraform/aws/_provider.tf
+++ terraform/aws/_provider.tf
@@ -1,22 +1,18 @@
 terraform {
-  backend "s3" {
-    bucket         = ""        # S3 bucket name
-    key            = ""        # Path to state file (e.g., env/dev/terraformtfstate)
-    region         = ""        # AWS region where bucket exists
-    dynamodb_table = ""        # (Optional but recommended) for state locking
-    encrypt        = true
-  }
-}
-
-terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.0.0"  # Adjust version as needed
+      version = "~> 5.0"
     }
+  }
+  backend "s3" {
+    bucket         = "terraformx-aws"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
   }
 }
 
 provider "aws" {
-  region = ""  # e.g., ap-south-1
+  region = var.region # Authentication via OIDC is handled in GitHub Actions, no keys here
 }
