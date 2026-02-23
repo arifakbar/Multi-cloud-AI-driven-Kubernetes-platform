23f8b262a38a8206dcfdab5671bff1d81471ce8f
Arif Akbar <arifakbarkv1@gmail.com>
2026-02-23 16:38:55 +0530

    Adding Azure VPN

--- terraform/aws/_variables.tf
+++ terraform/aws/_variables.tf
@@ -1,5 +1,5 @@
  variable "region" {
   description = "AWS region for resource deployment"
   type        = string
-  default     = "us-east-1"
+  default     = var.region
 }
---
+++ terraform/azure/vpn.tf
@@ -0,0 +1,33 @@
+#Gateway Subnet
+resource "azurerm_subnet" "gateway" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.255.224/27"]
+}
+
+#Public IP for VPN Gateway
+resource "azurerm_subnet" "gateway" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.255.224/27"]
+}
+
+#Virtual Network Gateway
+resource "azurerm_virtual_network_gateway" "vpn" {
+  name                = "azure-vpn-gateway"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+  sku      = "VpnGw1"
+
+  ip_configuration {
+    name                          = "vnetGatewayConfig"
+    public_ip_address_id          = azurerm_public_ip.vpn_pip.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.gateway.id
+  }
+}
