d656274e6e5cbdb4b3e7a1a28176c374ef8864d8
Arif Akbar <arifakbarkv1@gmail.com>
2026-02-24 21:08:23 +0530

    updated

--- terraform/aws/modules/vpc/rt.tf
+++ terraform/aws/modules/vpc/rt.tf
@@ -38,5 +38,5 @@ resource "aws_route_table_association" "private_assoc" {
   for_each = var.enable_nat_gateway ? aws_subnet.private : {}
 
   subnet_id      = each.value.id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private[0].id
 }
