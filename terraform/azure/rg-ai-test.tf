9eb602b16cab5b9c0212204f41f50e1c2d7670db
Arif Akbar <arifakbarkv1@gmail.com>
2026-02-28 15:39:05 +0530

    updated

--- terraform/azure/rg-ai-test.tf
+++ terraform/azure/rg-ai-test.tf
@@ -11,11 +11,11 @@ resource "azurerm_storage_account" "high_severity_sa" {
   account_replication_type = "LRS"
 
   # 🚨 HIGH (new attribute name)
-  allow_nested_items_to_be_public = true
+  allow_nested_items_to_be_public = false
 
   # 🚨 MEDIUM
-  min_tls_version = "TLS1_0"
+  min_tls_version = "TLS1_2"
 
   # 🚨 MEDIUM (new attribute name)
-  https_traffic_only_enabled = false
+  https_traffic_only_enabled = true
 }
